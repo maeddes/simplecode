1c2e54df2a8bb71e4ffd5e7183a230e5786d00c3
suedleasing <maeddes@gmail.com>
2020-07-16 18:55:03 +0200

    added logging

--- src/main/java/de/maeddes/simplecode/SimplecodeApplication.java
+++ src/main/java/de/maeddes/simplecode/SimplecodeApplication.java
@@ -2,7 +2,9 @@ package de.maeddes.simplecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.apache.commons.logging.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,8 @@ public class SimplecodeApplication {
 	@Value("${spring.profiles.active: none}")
 	String profile;
 
+	Logger logger = LoggerFactory.getLogger(SimplecodeApplication.class);
+
 	private String getInstanceId(){
 
 		if(!hostname.equals("not_set")) return hostname;
@@ -31,13 +35,15 @@ public class SimplecodeApplication {
 	@GetMapping("/hello")
 	String hello(){
 
-		return getInstanceId()+" Hello, OSSummit ! ";
+		logger.info("Call to hello method on instance: " + getInstanceId());
+		return getInstanceId()+" Hello, cf-for-k8s ! ";
 
 	}
 
 	@GetMapping("/fail")
 	String fail() {
 
+		logger.info("Call to failing method on instance: "+getInstanceId());
 		System.exit(1);
 		return "fixed!";
 	}
